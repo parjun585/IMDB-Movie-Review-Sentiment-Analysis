e9a4efd995ef201c587e8c8cd1710d1d101c28c7
Arjun P <141568567+parjun585@users.noreply.github.com>
2024-08-26 23:08:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
 
 # Load the pre-trained model with ReLU activation
-model = load_model('simpleRNN_IMDB_sentiment Analysis.h5')
+model = load_model('simpleRNN_IMDB_sentiment Analysis_part 2.h5')
 
 # Step 2: Helper Functions
 # Function to decode reviews
